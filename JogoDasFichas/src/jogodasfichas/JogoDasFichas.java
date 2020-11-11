d503ea2214d258154f91ed49f475d5c9a9d372c8
Rodrigo Oliveira Silva <rodrigo.silvoliveira@gmail.com>
2020-11-11 10:29:40 -0300

    V 0.4

--- JogoDasFichas/src/jogodasfichas/JogoDasFichas.java
+++ JogoDasFichas/src/jogodasfichas/JogoDasFichas.java
@@ -7,69 +7,156 @@ public class JogoDasFichas {
 
     static ArrayList<ArrayList<Integer>> estadosGerados = new ArrayList();
 
+    public static void imprimeGerados() {
+
+        System.out.println("Gerados: " + estadosGerados.size());
+
+        for (int i = 0; i < estadosGerados.size(); i++) {
+
+            System.out.println(estadosGerados.get(i));
+        }
+
+    }
+
+    public static ArrayList criaArray(int tamanho) {
+        ArrayList<Integer> array = new ArrayList();
+
+        int metade = tamanho / 2;
+
+        for (int i = 0; i < metade; i++) {
+
+            array.add(1);
+        }
+
+        for (int i = 0; i < metade; i++) {
+
+            array.add(2);
+
+        }
+
+        array.add(0);
+
+        Collections.shuffle(array);
+
+        return array;
+    }
+
     public static void imprimeCaminho(Vertice folha) {
+
         ArrayList<ArrayList<Integer>> caminho = new ArrayList();
+
         while (folha != null) {
+
             caminho.add(folha.getEstado());
+
             folha = folha.getPai();
         }
-        
-        for(int i = caminho.size()-1 ; i>-1;i--){
+
+        for (int i = caminho.size() - 1; i > 0; i--) {
+
             System.out.println(caminho.get(i));
+
         }
+
+        System.out.println("");
+
+        System.out.println("Caminho tem " + caminho.size() + " estados.");
+
     }
 
     public static boolean ehSolucao(ArrayList l, ArrayList solucao) {
+
         ArrayList<Integer> lCopia = new ArrayList();
+
         ArrayList<Integer> aux = l;
+
         ArrayList<Integer> solucaoCopia = new ArrayList();
+
         ArrayList<Integer> aux2 = solucao;
 
         for (int i = 0; i < l.size(); i++) {
+
             lCopia.add(aux.get(i));
+
             solucaoCopia.add(aux2.get(i));
+
         }
 
+        lCopia.remove(0);
+
+        solucaoCopia.remove(0);
+
         return lCopia.equals(solucaoCopia);
+        //return l.equals(solucao);
+
     }
 
     public static void addEstadoGerados(Vertice atual) {
+
         ArrayList<Integer> aux = new ArrayList();
+
         for (int i = 0; i < atual.getEstado().size(); i++) {
+
             aux.add(atual.getEstado().get(i));
         }
+
         estadosGerados.add(aux);
     }
 
     public static boolean verificaArvore(Vertice atual) {
 
         for (int i = 0; i < estadosGerados.size(); i++) {
+
             if (estadosGerados.get(i).equals(atual.getEstado())) {
+
                 return false;
+
             }
+
         }
+
         return true;
+
     }
 
     public static ArrayList<Integer> troca(ArrayList l, int pos) {
+
         int posVazio = l.indexOf(0);
+
         Collections.swap(l, posVazio, pos);
+
         return l;
+
     }
 
     public static boolean existeRegra(ArrayList l, int salto, Vertice atual) {
+
         int posVazio = l.indexOf(0);
+
         for (int i = posVazio - salto; i <= posVazio + salto; i++) {
+
             if (i >= 0 && i < l.size()) {
-                troca(l, i);
+
+                //troca(l, i);
+                Collections.swap(l, posVazio, i);
+
                 if (verificaArvore(atual)) {
+
                     return true;
+
                 } else {
-                    troca(l, i);
+
+                    //troca(l, posVazio);
+                    Collections.swap(l, i, posVazio);
+
                 }
+
             }
+
         }
+
         return false;
+
     }
 
     /* public static void irrevogavel(ArrayList inicio, ArrayList solucao, int salto) {
@@ -115,10 +202,13 @@ public class JogoDasFichas {
 
         Vertice aux = raiz;
 
+        if (ehSolucao(inicio, solucao)) {
+            sucesso = true;
+        }
+
         while (fracasso != true && sucesso != true) {
 
             //imprimeGerados();
-
             N = aux.getEstado();
 
             if (existeRegra(N, salto, aux)) { //R(N) != VAZIO
@@ -153,36 +243,68 @@ public class JogoDasFichas {
 
         }
 
-        System.out.println("Sucesso: " + sucesso);
-        System.out.println("Fracasso: " + fracasso);
-        System.out.println(inicio);
-        imprimeCaminho(aux);
-    }
+        if (sucesso) {
 
-    /*  public static void buscaLargura(Vertice v, ArrayList solucao) {
-        //abertos = Cria uma fila com estados abertos
-        Vertice S = v;
-        Vertice N = v;
-        boolean fracasso = false, sucesso = false;
-        //insere S em abertos
-        while (!fracasso || !sucesso) {
-            if (1 == 1) { //abertos = vazio
-                fracasso = true;
-            } else {
-                //N = primeiro de abertos
-                if (ehSolucao(N.getEstado(), solucao)) {
-                    sucesso = true;
-                } else {
-                    while (1 == 1) {//R(N) != VAZIO
-                        ///  
-                    }
-                    ///
-                }
-            }
+            System.out.println("Movimentos realizados para atingir o objetivo:");
+
+            System.out.println(inicio);
+
+            imprimeCaminho(aux);
+        }
+
+        if (fracasso) {
+
+            System.out.println("Fracasso");
         }
     }
 
-    public static void buscaProfundidade(Vertice v, ArrayList solucao) {
+    public static void buscaLargura(ArrayList inicio, ArrayList solucao, int salto) {
+        ArrayList<Vertice> abertos = new ArrayList();
+        ArrayList<Vertice> fechados = new ArrayList();
+        Vertice raiz = new Vertice(null, inicio);
+        addEstadoGerados(raiz);
+        //ArrayList<Integer> S = inicio;
+        ArrayList<Integer> N = new ArrayList();
+        boolean fracasso = false, sucesso = false;
+        abertos.add(raiz);
+        Vertice aux = abertos.get(0);
+        while (fracasso != true && sucesso != true) {
+            if (abertos.isEmpty()) { //abertos = vazio
+                fracasso = true;
+            } else {
+                //N = abertos.get(0).getEstado();
+                //Collections.copy(N, abertos.get(0));
+                N = aux.getEstado();
+                if (ehSolucao(N, solucao)) {
+                    sucesso = true;
+                } else {
+                    while (existeRegra(N, salto, aux)) {//R(N) != VAZIO
+                        addEstadoGerados(aux);
+                        Vertice aux2 = new Vertice(aux, N);
+                        aux.addFilho(aux2);
+                        //ArrayList<Integer> u = new ArrayList<>(N);
+                        //Collections.copy(u, N);
+                        abertos.add(aux2);
+                        // Atualiza R(N)
+                        //aux = aux2;
+                    }
+                    fechados.add(aux);
+                    abertos.remove(0);
+                    aux = abertos.get(0);
+                }
+            }
+        }
+        if (sucesso) {
+            System.out.println("Movimentos realizados para atingir o objetivo:");
+            System.out.println(inicio);
+            imprimeCaminho(aux);
+        }
+        if (fracasso) {
+            System.out.println("Fracasso");
+        }
+    }
+
+    /*public static void buscaProfundidade(Vertice v, ArrayList solucao) {
         //abertos = Cria uma pilha com estados abertos
         Vertice S = v;
         Vertice N = v;
@@ -228,34 +350,58 @@ public class JogoDasFichas {
             }
         }
     }*/
-    public static void imprimeGerados() {
-        System.out.println("Gerados: " + estadosGerados.size());
-        for (int i = 0; i < estadosGerados.size(); i++) {
-            System.out.println(estadosGerados.get(i));
-        }
-    }
-
     public static void main(String[] args) {
-
-        ArrayList<Integer> I = new ArrayList();
-
-        ArrayList<Integer> S = new ArrayList();
-
-        I.add(1);
-        I.add(2);
-        I.add(0);
-        I.add(2);
-        I.add(1);
-
-        S.add(2);
-        S.add(2);
-        S.add(0);
-        S.add(1);
-        S.add(1);
 
         int salto = 2;
 
+        int tamanho = 5;
+
+        ArrayList<Integer> I = criaArray(tamanho);
+
+        ArrayList<Integer> S = criaArray(tamanho);
+
+        /*ArrayList<Integer> I = new ArrayList();
+        
+        ArrayList<Integer> S = new ArrayList();
+        
+        I.add(2);
+        I.add(2);
+        I.add(0);
+        I.add(1);
+        I.add(1);
+        
+        S.add(1);
+        S.add(1);
+        S.add(2);
+        S.add(0);
+        S.add(2);*/
+        System.out.print("Saltos: " + salto);
+
+        System.out.println(" Tamanho do vetor: " + tamanho);
+
+        System.out.println("Inicio:   " + I);
+
+        System.out.println("Objetivo: " + S);
+
+        System.out.println("");
+
+        /*long startTime = System.currentTimeMillis();
+
         backtraking(I, S, salto);
 
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Foram Gerados " + estadosGerados.size() + " estados no total.");
+
+        System.out.println("");
+
+        System.out.println("Tempo de execução: " + (endTime - startTime) + " ms.");*/
+        buscaLargura(I, S, salto);
+
+        for (int i = 0; i < estadosGerados.size(); i++) {
+            System.out.println(estadosGerados.get(i));
+        }
+
+        //backtraking(I, S, salto);
     }
 }
